0db1966d376b38a42e2eb77361c02dca524400a9
Joe Reeve <joe@wigwamm.com>
2015-05-04 16:05:38 +0100

    Refactor

--- Taken.js
+++ Taken.js
@@ -1,23 +1,26 @@
-var T = function(id, fn) {
-  return fn = {
+var T = function(id, fn, helpers) {
+  fn = function(value) {
+    T.channels[id].forEach(function(handlerSet) {
+      fn.genHandler(handlerSet)(value);
+    });
+  };
+  Object.keys(helpers = {
     handlerSet: (T.channels[id] = (T.channels[id] || []).concat([[]])).slice(-1)[0],
-    genHandler: function(allHandlers){
+    genHandler: function(allHandlers) {
       return function(value) {
-        (allHandlers[0]||Object)(value, function(value) {
+        (allHandlers[0] || Object)(value, function(value) {
           fn.genHandler(allHandlers.slice(1))(value);
         });
       };
-    },
-    pub: function(value) {
-      T.channels[id].forEach(function(handlerSet) {
-        fn.genHandler(handlerSet)(value);
-      });
     },
     map: function(handler) {
       fn.handlerSet.push(handler);
       return fn;
     }
-  }
+  }).map(function(key) {
+    fn[key] = helpers[key];
+  });
+  return fn;
 };
 T.channels = {};
 module.exports = T;
--- demo.js
+++ demo.js
@@ -1,7 +1,12 @@
 var T = require('./Taken.js');
 
-T('test').
+T('xyz').
   map(function(val, next){val.num = val.num+1;next(val);}).
-  map(function(val, next){console.log(val.num, val.val);next(val)});
+  map(function(val, next){console.log(val.num, val.val);});
 
-T('test').pub({val:'hi', num:1});
+T('xyz')({val:'hi', num:1});
+
+
+var Test = T().map(function(val){console.log(val)});
+
+Test('hi');
